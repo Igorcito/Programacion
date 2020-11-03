5d5ffc46ba75741755c65681a3c948d9bfb5d99a
david <david@DESKTOP-Q872S7T.home>
2020-11-03 22:28:48 +0100

    Programa hola soy david

---
+++ Programación de Java/src/UT3Programación/holasoydavid.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package UT3Programación;
+
+/**
+ * @author david
+ *
+ */
+public class holasoydavid {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola clase, soy David.");
+	}
+
+}
